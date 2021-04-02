ecd11dc36660ca8581fffd4a9104b2fce22dba40
Nahl <nahlultra@gmail.com>
2021-04-02 21:20:36 +0700

    yey

---
+++ android/app/src/main/kotlin/com/example/muhammad_bin_sholih_alutsaimin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.muhammad_bin_sholih_alutsaimin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/home.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: DefaultTextStyle(
+          style: GoogleFonts.workSans(color: Colors.black),
+          child: ListView(
+            padding: EdgeInsets.only(right: 24, left: 24, top: 64, bottom: 0),
+            children: [
+              Text("Muhammad bin Sholih Al Utsaimin",
+                  style: GoogleFonts.workSans(
+                      fontWeight: FontWeight.bold, fontSize: 42)),
+              Text("Biografi", style: TextStyle(fontSize: 24)),
+              SizedBox(height: 64),
+              Text('''Nasabnya
+Beliau adalah Abu Abdillah, Muhammad bin Sholih Al Utsamin, Al Wuhaibi, At Tamimi.
+
+Kelahirannya
+Beliau dilahirkan di kota ‘Unaizah pada tanggal 27 Ramadhan tahun 1347 H.
+
+Pertumbuhannya
+Beliau belajar al-Qur’an pada kakeknya dari jalur ibunya, Abdurrahman bin Sulaiman Alu Damigh rahimahullah, kemudianmenghafalnya. Setelah itu beliau mulai belajar khat (menulis), ilmu hitung, dan sebagian cabang ilmu sastra.
+
+Syaikh Abdurrahman bin Naashir As Sa’di mengangkat dua orang muridnya untuk mengajar penuntut ilmu yunior yaitu syaikh Ali As Shalihi dan syaikh Muhammad bin Abdul ‘Aziz Al Muthawi’ rahimahullah. Kepadanya syaikh Utsaimin belajar kitab Mukhtashar al Aqidah Al Wasithiyah karya Syaikh Abdurrahman As Sa’di, kitab Minhaj as Salikin fil Fiqh karya syaikh Abdurrahman As Sa’di, Kitab Al Ajrumiyah dan al Alfiyah. Beliau belajar faraid (ilmu waris) dan fiqih kepada Syaikh Abdurrahman bin ‘Ali bin ‘Audan.
+
+Beliau belajar kepada Syaikh Abdurrahman bin Naashir As Sa’di yang beliau anggap sebagai syaikh pertamanya. Beliau bermulazamah kepadanya, belajar ilmu Tauhid, Tafsir, Hadits, Fiqih, Ushul Fiqih, Faraid, Musthalah al Hadits, Nahwu dan Sharaf.
+
+Beliau memiliki kedudukan yang khusus di sisi Syaikh As Sa’di, sehingga ketika orang tua beliau pindah ke Riyad, orang tuanya menginginkan agar beliau ikut pindah padahal saat itu adalah awal perkembangannya, maka Syaikh Abdurrahman As Sa’di rahimahullah, menulis surat kepada orang tuanya yang di antara isinya, “Hal ini tidak mungkin, kami ingin agar Muhammad tinggal di sini supaya tetap belajar.”
+
+Syaikh Muhammad bin Shalih al Utsaimin mengatakan, “Sungguh, saya banyak terpengaruh dengan beliau dalam metode pengajaran, pemaparan ilmu, serta pendekatannya terhadap penuntut ilmu dengan memberikan contoh-contoh dan makna-makna. Demikian juga saya terkesan terhadap beliau dari sisi akhlaknya, beliau memiliki akhlak yang mulia, beliau memiliki kedudukan yang tinggi di dalam hal ilmu dan ibadah, beliau mencandai anak-anak kecil serta tertawa kepada yang besar. Beliau termasuk di antara orang yang paling baik akhlaknya yang pernah saya lihat.”
+
+Syaikh Utsaimin juga belajar kepada Syaikh Abdul ‘Aziz bin Baz yang beliau anggap sebagai syaikhnya yang kedua. Beliau memulainya dengan belajar Shahih Bukhari, sebagian risalah Syaikhul Islam Ibnu Taimiyah dan beberapa kitab fikih. Beliau mengatakan, “Aku terkesan dengan Syaikh ‘Abdul ‘Aziz bin Baz Hafizhahullah tentang perhatiannya terhadap hadits, akhlaknya serta kelapangan jiwanya terhadap orang lain.”
+
+Pada tahun 1371 H beliau mengajar di masjid Jami’. Ketika Ma’had ‘Ilmiyah didirikan di Riyadh beliau memasukinya pada tahun 1372, beliau mengatakan, “Saya memasuki ma’had ‘Ilmi pada tahun kedua, saya memasukinya atas saran dari syaikh ‘Ali as Shalihi setelah saya meminta izin kepada Syaikh ‘Abdurrahman As Sa’di, semoga Allah merahmatinya. Pada waktu itu ma’had ‘ilmi terbagi menjadi dua bagian yaitu khusus dan umum, sedangkan saya masuk pada bagian khusus. Pada waktu itu juga siapa saja yang menginginkan maka bisa ‘melompat’, demikian mereka menyebutnya, maksudnya seseorang belajar pelajaran kelas tingkat di atasnya pada waktu liburan kemudian mengikuti ujian pada awal tahun kedua, jika ia lulus ia boleh pindah ke kelas di atasnya sehingga dengan demikian masa studi bisa lebih singkat.
+
+Setelah dua tahun beliau lulus dan ditetapkan sebagai pengajar di Ma’had ‘Unaizah al ‘Ilmi sambil melanjutkan kuliah jarak jauh pada fakultas syari’ah serta melanjutkan menuntut ilmu pada Syaikh ‘Abdurrahman As Sa’di. Ketika Syaikh ‘Abdurrahman as Sa’di rahimahullah meninggal dunia beliau diangkat menjadi imam Masjid Jami’ al Kabir di ‘Unaizah dan mengajar di Perpustakaan Nasional di samping mengajar di Ma’had ‘al ‘Ilmi. Kemudian beliau pindah untuk mengajar di fakultas syari’ah dan ushuluddin Universitas Imam Muhammad bin Su’ud al islamiyah cabang Qosim. Selain sebagai anggota Haiah Kibarul ‘Ulama di kerajaan Arab Saudi, beliau memiliki semangat dan aktivitas yang besar dalam berdakwah kepada Allah ‘azza wa jalla dan membimbing para da’i di berbagai tempat. Beliau juga memiliki perjuangan yang berharga pada medan dakwah.
+
+Sehingga sangat layak untuk disebutkan juga bahwa syaikh Muhammad bin Ibrahim rahimahullah pernah menawari bahkan mendesak beliau untuk menjadi qodhi(hakim) bahkan telah mengeluarkan Keputusan dengan menetapkan beliau hafizhahullah sebagai kepala Mahkamah Syari’ah di Ihsa’ namun beliau meminta untuk dibebaskan tugaskan dari tugas tersebut. Setelah adanya pertimbangan-pertimbangan dan pendekatan personal dari Syaikh maka beliau diizinkan untuk dibebaskan dari jabatan sebagai hakim.
+
+Karya-Karyanya
+Beliau memiliki tulisan yang banyak mencapai 40 berupa kitab dan risalah yang akan dikumpulkan –insya Allah– dalam Majmu al Fatawa wa ar Rasail.
+
+Sumber: Syarhu Kasyfu Asy Syubuhat, Penerbit Daarul Kutubil ‘Ilmiyah
+
+
+
+Simak selengkapnya disini. Klik https://muslim.or.id/53-biografi-ringkas-syaikh-muhammad-bin-sholih-al-utsaimin.html'''),
+              SizedBox(
+                height: 42,
+              ),
+              Wrap(
+                alignment: WrapAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Container(
+                      color: Color.fromRGBO(0, 0, 0, 0.04),
+                      padding: EdgeInsets.only(
+                          right: 12, left: 12, top: 8, bottom: 8),
+                      child: Text(
+                        "nahlmb.dev - 2020 - IDN Final Task\nsumber : muslim.or.id",
+                        style: TextStyle(color: Colors.black54),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 42,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
